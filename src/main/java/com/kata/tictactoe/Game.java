0c9ed1bf6348f9c547c6e11ed54248d82dfc826a
David Meszaros <david.mark.meszaros@ibm.com>
2021-08-01 13:19:57 +0200

    After O the next mark should be X - GREEN

--- src/main/java/com/kata/tictactoe/Game.java
+++ src/main/java/com/kata/tictactoe/Game.java
@@ -6,6 +6,8 @@ public class Game {
     public void placeMark(int i) {
         if(status == Status.X_NEXT) {
             status = Status.O_NEXT;
+        } else if(status == Status.O_NEXT) {
+            status = Status.X_NEXT;
         }
     }
 
