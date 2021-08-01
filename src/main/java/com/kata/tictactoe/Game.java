ad05c852ff8fc912437b791a0d0848f9cde9c8e0
David Meszaros <david.mark.meszaros@ibm.com>
2021-08-01 14:06:52 +0200

    Print board - GREEN

--- src/main/java/com/kata/tictactoe/Game.java
+++ src/main/java/com/kata/tictactoe/Game.java
@@ -7,7 +7,11 @@ public class Game {
     private Status status = Status.X_NEXT;
 
     public void print() {
-
+        System.out.println(board[0] + "|" + board[1] + "|" + board[2]);
+        System.out.println("-+-+-");
+        System.out.println(board[3] + "|" + board[4] + "|" + board[5]);
+        System.out.println("-+-+-");
+        System.out.println(board[6] + "|" + board[7] + "|" + board[8]);
     }
 
     public void placeMark(int i) {
--- src/main/java/com/kata/tictactoe/Mark.java
+++ src/main/java/com/kata/tictactoe/Mark.java
@@ -1,5 +1,16 @@
 package com.kata.tictactoe;
 
 public enum Mark {
-    X, O, EMPTY
+    X("X"), O("O"), EMPTY(" ");
+
+    private final String value;
+
+    Mark(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
