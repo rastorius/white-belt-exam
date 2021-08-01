3b0b7db7c62fc22e17830d0fb053a57ebc71a31e
David Meszaros <david.mark.meszaros@ibm.com>
2021-08-01 14:36:23 +0200

    Print O placement should be printed in the right format - RED

--- src/test/java/com/kata/tictactoe/BotTest.java
+++ src/test/java/com/kata/tictactoe/BotTest.java
@@ -110,4 +110,34 @@ class BotTest {
         System.setOut(originalOut);
     }
 
+    @Test
+    @DisplayName("Print O placement should be printed in the right format")
+    void givenOneMark_whenMarkWithO_thenShouldPrintOPlacement() {
+        // given
+        IntSupplier intSupplier = mock(IntSupplier.class);
+        when(intSupplier.getAsInt()).thenReturn(0).thenReturn(1);
+        Bot bot = new Bot(intSupplier, new Game());
+        bot.mark();
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        final PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        String eol = System.lineSeparator();
+        String expectedOutput =
+                "Player O:" + eol +
+                        "X|O| " + eol +
+                        "-+-+-" + eol +
+                        " | | " + eol +
+                        "-+-+-" + eol +
+                        " | | " + eol;
+
+        // when
+        bot.mark();
+
+        // then
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+
+        System.setOut(originalOut);
+    }
 }
